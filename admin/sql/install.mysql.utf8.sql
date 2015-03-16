9026bc418ca4bfe7e80cb59c8d07f9b9f4d09f9b
spacecadet9 <danielhpavey@gmail.com>
2015-03-16 16:03:49 +0000

    Update sql

--- admin/sql/install.mysql.utf8.sql
+++ admin/sql/install.mysql.utf8.sql
@@ -8,3 +8,23 @@ ALTER TABLE #__styleguide
 
 ALTER TABLE #__styleguide
     MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+
+INSERT INTO #_styleguide ( 'content' )
+values (
+'<h1>H1 Header</h1>
+<h2>H2 Header</h2>
+<h3>H3 Header</h3>
+<h4>H4 Header</h4>
+
+<p>This is a paragraph. Vestibulum interdum odio dictum sodales rutrum. Etiam pellentesque tortor in tellus semper porta. Suspendisse finibus ut augue dictum molestie. Pellentesque pulvinar dolor dolor, et tincidunt orci rutrum vitae. Etiam lacinia efficitur nunc, a efficitur libero gravida eu. Phasellus lobortis magna ac massa varius elementum. Phasellus sit amet lorem justo. Suspendisse non nisl dolor. Quisque dapibus nisi tellus, lacinia porttitor elit bibendum nec.</p>
+
+<p>This is another paragraph. Vestibulum interdum odio dictum sodales rutrum. Etiam pellentesque tortor in tellus semper porta. Suspendisse finibus ut augue dictum molestie. Pellentesque pulvinar dolor dolor, et tincidunt orci rutrum vitae. Etiam lacinia efficitur nunc, a efficitur libero gravida eu. Phasellus lobortis magna ac massa varius elementum. Phasellus sit amet lorem justo. Suspendisse non nisl dolor. Quisque dapibus nisi tellus, lacinia porttitor elit bibendum nec.</p>
+
+<ul>
+<li>List Item</li>
+<li>List Item</li>
+<li>List Item</li>
+<li>List Item</li>
+<li>List Item</li>
+</ul>'
+);
